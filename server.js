ac723183ad21d95d8193bc4ac024ec7764060ec1
B-Musick <bmuze1@gmail.com>
2019-10-30 11:30:16 -0600

    Styled the books page, added header and footer, UPDATE route works

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ let express = require('express'),
     bodyParser = require('body-parser'),
     mysql   = require('mysql')
     morgan = require('morgan')
-    dotenv = require('dotenv');
+    dotenv = require('dotenv')
+    methodOverride = require('method-override');
 
 // Set up the .env file to access through process.env.VALUE
 dotenv.config();
@@ -18,6 +19,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 // Sets the extension 
 app.set('view engine', 'ejs');
+
+// METHOD-OVERRIDE (PUT form action) ?_method=PUT
+app.use(methodOverride('_method'));
 
 /*************************** DATABASE CONNECTION ******************************/
 getConnection=()=>{ 
@@ -60,13 +64,29 @@ app.get('/',(req,res)=>{
 });
 
 /************************** ALTER TABLE **************************************/
-// app.get('/books/addAttr',(req,res)=>{
-//     res.render('/books/addAttr');
-// });
+app.get('/books/addAttr',(req,res)=>{
+    res.render('books/addAttr');
+});
 
-// app.post('/books/', (req, res) => {
-//     res.render('/books/addAttr');
-// });
+app.post('/books/addAttr', (req, res) => {
+    let column = req.body.column;
+    let dataType = req.body.dataType;
+    let constraint = req.body.constraint;
+    let defaultVal = req.body.defaultVal;
+
+    
+    let queryString = `ALTER TABLE books ADD ${column} ${dataType};`;
+    // The values in the brackets are inserted into the query string (?,?)
+    connection.query(queryString, [], (err, results, fields) => {
+        if (err) {
+            console.log('Failed to alter books table '+ err);
+            res.sendStatus(500);
+            return
+        }
+        console.log('Updated books table ' + results);
+        res.redirect('/books');
+    });
+});
 
 /*************************** SELECT ******************************************/
 makeSelectQuery = (queryString, queryVariables, res, connection, route) => {
@@ -83,9 +103,10 @@ makeSelectQuery = (queryString, queryVariables, res, connection, route) => {
 
         // Rename the values from SQL for JSON
         let books = rows.map(row => {
-            return { title: row.title, author: row.author }
+            return { title: row.title, author: row.author, image: row.Image }
         });
 
+        
         res.render(route, {books});
     })
 }; // makeSelectQuery()
@@ -105,6 +126,7 @@ app.get('/books/:title&:author',(req,res)=>{
     // Get the show page for the book
     let title = req.params.title;
     let author = req.params.author;
+    
 
     const queryString = "SELECT * FROM books WHERE title LIKE ? AND author LIKE ?";
     
@@ -120,9 +142,16 @@ app.get('/books/:title&:author',(req,res)=>{
             return
         }
         // Rename the values from SQL for JSON   
-        console.log(rows);
-
-        res.render('books/show', {author,title});
+        // console.log(rows);
+        let book = rows.map(book=>{
+            if(book.title === title){
+                
+                return book;
+            }
+        });
+        let image = book[0].Image;
+        
+        res.render('books/show', {author,title, image});
         
     })
     
@@ -135,11 +164,11 @@ app.get('/books/create',(req,res)=>res.render('books/create'));
 app.post('/books', (req,res)=>{
     let title = req.body.title;
     let author = req.body.author;
-    // let id      = req.body.id;
+    let image     = req.body.image;
     console.log(req.body);
 
-    let queryString = 'INSERT INTO books (title, author) VALUES (?,?)';
-    connection.query(queryString, [title,author],(err,results,fields)=>{
+    let queryString = 'INSERT INTO books (title, author,image) VALUES (?,?,?)';
+    connection.query(queryString, [title,author,image],(err,results,fields)=>{
         if (err) {
             console.log('Failed to input new book.');
             res.sendStatus(500);
@@ -148,7 +177,57 @@ app.post('/books', (req,res)=>{
         console.log('Inserted a new book ' + results.insertId);
         res.redirect('/books');
     })
-})
+});
+
+/**************************** UPDATE ****************************************/
+
+// LOAD EDIT PAGE FOR UPDATE ROUTE
+app.get('/books/:title&:author/update', (req, res) => {
+    let title = req.params.title;
+    let author = req.params.author;
+
+    const queryString = "SELECT * FROM books WHERE author LIKE ? AND title LIKE ?";
+    connection.query(queryString, [author,title], (err, rows, fields) => {
+        if (err) {
+            console.log('failed to query user' + err)
+            res.sendStatus(500);
+            // throw err (can do this istead)
+            res.end()
+            return
+        }
+        let book = rows.map(book => {
+            if (book.title === title) {
+                return book;
+            }
+        });
+        
+        // rows returns [{user}] so need to take it out of the array
+        res.render('books/update', { book:rows[0] });
+    });
+});
+
+// UPDATE ROUTE
+app.put('/books/:title&:author', (req, res) => {
+
+    let title = req.params.title;
+    let author = req.params.author;
+    let image = req.body.image;
+
+  
+    let queryString = 'UPDATE books SET author= ?, title= ? , Image= ? WHERE author LIKE ? AND title LIKE ?';
+    // The values in the brackets are inserted into the query string (?,?)
+
+    connection.query(queryString, [author, title, image,author,title], (err, results, fields) => {
+        if (err) {
+            console.log('Failed to update book');
+            res.sendStatus(500);
+            return
+        }
+        console.log('Updated a book ' + results);
+        res.redirect(`/books/${title}&${author}`);
+    })
+});
+
 
 app.listen(process.env.PORT,()=>{
     console.log(`Server is running and listening on ${process.env.PORT}...`);
